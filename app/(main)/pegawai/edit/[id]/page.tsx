4f8ae4318a7b87e3fc67bab7982d2bfda3011c41
ilhamTq <ilhamtaufiq179@gmail.com>
2024-07-27 14:42:04 +0800

    perbaikan

--- app/(main)/pegawai/edit/[id]/page.tsx
+++ app/(main)/pegawai/edit/[id]/page.tsx
@@ -1,27 +1,27 @@
-import FormUser from '@/components/apps/mailbox/invoice/FormUser';
-import prisma from '@/lib/prisma';
-import { Metadata } from 'next';
-import React from 'react';
+import FormUser from "@/components/apps/mailbox/invoice/FormUser";
+import prisma from "@/lib/prisma";
+import { Metadata } from "next";
+import React from "react";
 
 export const metadata: Metadata = {
-    title: 'Edit Pegawai',
+  title: "Edit Pegawai",
 };
 
 interface PegawaiProps {
-    params?: {
-        id?: number;
-    };
+  params?: {
+    id?: number;
+  };
 }
 
 const getPegawai = async (id: number) => {
-    return await prisma.users.findUnique({ where: { user_id: id }, include: { bank: true } });
+  return await prisma.users.findUnique({ where: { user_id: id } });
 };
 
 const PegawaiAdd = async ({ params }: PegawaiProps) => {
-    const data = await getPegawai(Number(params?.id));
-    // console.log(data);
-    if (data) data.user_pass = '';
-    return <FormUser data={data} />;
+  const data = await getPegawai(Number(params?.id));
+  // console.log(data);
+  if (data) data.user_pass = "";
+  return <FormUser data={data} />;
 };
 
 export default PegawaiAdd;
